7a66457b1749dca289bdec9f31c1fb67d49066d6
sggyflt <sgflt88@gmail.com>
2024-09-03 17:46:14 +0200

    feat: add functionality to Form component

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,18 +1,36 @@
+import {useState} from "react";
+
 function Form() {
+
+  const [car, setCar] = useState({year: 1963, manufacturer: "Buick", model: "Riviera"});
+
+  function handleYearChange(event){
+    setCar(c => ({...c, year: event.target.value}));
+  }
+
+  function handleManufacturerChange(event){
+    setCar(c => ({...c, manufacturer: event.target.value}));
+  }
+
+  function handleModelChange(event){
+    setCar(c => ({...c, model: event.target.value}));
+  }
+
   return(
     <div>
       <h1>Choose your favourite car model:</h1>
+      <p>You have chosen: {car.year} {car.manufacturer} {car.model}</p>
       <div>
         <label for="year">Year:</label>
-        <input id="year" type="number" placeholder="1963" />
+        <input id="year" type="number" value={car.year} onChange={handleYearChange} />
 
         <label for="manufacturer">Manufacturer:</label>
-        <input id="manufacturer" type="text"  placeholder="Buick" />
+        <input id="manufacturer" type="text" value={car.manufacturer} onChange={handleManufacturerChange} />
 
         <label for="model">Model:</label>
-        <input id="model" type="text" placeholder="Riviera" />
+        <input id="model" type="text" value={car.model} onChange={handleModelChange} />
       </div>
-      <h2>{`You have chosen`}</h2>
+      
     </div>
   )
 }
